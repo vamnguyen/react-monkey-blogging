79fbce3cfc020a7abafad31f4c27995ee584cb0a
Nguyễn Viết Anh Minh <anhminh.dev@gmail.com>
2023-06-30 21:57:16 +0700

    Add Dropdown, Checkbox, Radio UI

--- src/module/post/PostAddNew.js
+++ src/module/post/PostAddNew.js
@@ -5,12 +5,23 @@ import { Label } from "components/label";
 import React from "react";
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
+import { Checkbox, Radio } from "components/checkbox";
+import { Dropdown } from "components/dropdown";
+
 const PostAddNewStyles = styled.div``;
 
 const PostAddNew = () => {
-  const { control, handleSubmit } = useForm({
+  const { control, watch, setValue } = useForm({
     mode: "onChange",
+    defaultValues: {
+      status: "",
+      category: "",
+    },
   });
+  const watchStatus = watch("status");
+  const watchCategory = watch("category");
+  console.log("PostAddNew ~ watchCategory", watchCategory);
+
   return (
     <PostAddNewStyles>
       <h1 className="dashboard-heading">Add new post</h1>
@@ -36,14 +47,51 @@ const PostAddNew = () => {
         <div className="grid grid-cols-2 gap-x-10 mb-10">
           <Field>
             <Label>Status</Label>
+            <div className="flex items-center gap-x-5">
+              <Radio
+                name="status"
+                control={control}
+                checked={watchStatus === "approved"}
+                onClick={() => setValue("status", "approved")}
+                value="approved"
+              >
+                Approved
+              </Radio>
+              <Radio
+                name="status"
+                control={control}
+                checked={watchStatus === "pending"}
+                onClick={() => setValue("status", "pending")}
+                value="pending"
+              >
+                Pending
+              </Radio>
+              <Radio
+                name="status"
+                control={control}
+                checked={watchStatus === "reject"}
+                onClick={() => setValue("status", "reject")}
+                value="reject"
+              >
+                Reject
+              </Radio>
+            </div>
           </Field>
           <Field>
             <Label>Author</Label>
+            <Input control={control} placeholder="Find the author"></Input>
           </Field>
         </div>
         <div className="grid grid-cols-2 gap-x-10 mb-10">
           <Field>
             <Label>Category</Label>
+            <Dropdown>
+              <Dropdown.Option>Knowledge</Dropdown.Option>
+              <Dropdown.Option>Blockchain</Dropdown.Option>
+              <Dropdown.Option>Setup</Dropdown.Option>
+              <Dropdown.Option>Nature</Dropdown.Option>
+              <Dropdown.Option>Developer</Dropdown.Option>
+            </Dropdown>
           </Field>
           <Field></Field>
         </div>
