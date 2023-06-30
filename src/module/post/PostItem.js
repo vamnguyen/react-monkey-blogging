0b1fd6b750865beb125e0dc93dff66daf3c1c23b
Nguyễn Viết Anh Minh <anhminh.dev@gmail.com>
2023-06-30 20:37:23 +0700

    Optimize UI for cleaner

--- src/module/post/PostItem.js
+++ src/module/post/PostItem.js
@@ -17,10 +17,10 @@ const PostItemStyles = styled.div`
       border-radius: 16px;
     }
     &-category {
-      margin-bottom: 16px;
+      margin-bottom: 10px;
     }
     &-title {
-      margin-bottom: 8px;
+      margin-bottom: 20px;
     }
   }
   @media screen and (max-width: 1023.98px) {
